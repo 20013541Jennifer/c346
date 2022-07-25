b2abf3f1dfadf2c84228828efd3856fc06042ad3
20013541Jennifer <104333117+20013541Jennifer@users.noreply.github.com>
2022-07-25 21:48:58 +0800

    Update ViewActivity.java

--- srcL9ps/src/main/java/com/myapplicationdev/android/ndpsongs/ViewActivity.java
+++ srcL9ps/src/main/java/com/myapplicationdev/android/ndpsongs/ViewActivity.java
@@ -4,13 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
 public class ViewActivity extends AppCompatActivity {
 
+    Button btnView;
     ArrayList<Song> songs;
     ListView lv;
     ArrayAdapter<Song> aa;
@@ -21,16 +25,50 @@ public class ViewActivity extends AppCompatActivity {
         setContentView(R.layout.activity_view);
 
         lv = findViewById(R.id.lv);
+        btnView = findViewById(R.id.btnView);
+
+        songs = new ArrayList<Song>();
+        aa = new ArrayAdapter<Song>(this,
+                android.R.layout.simple_list_item_1, songs);
+
 
         DBHelper dbh = new DBHelper(this);
 
         songs= dbh.getAllSongs();
-       songs = dbh.getAllSongsByStars(5);
-       ArrayAdapter<Song> adapter = new ArrayAdapter<Song>(this,
+       aa = new ArrayAdapter<Song>(this,
                 android.R.layout.simple_list_item_1, songs);
-        lv.setAdapter(adapter);
+        lv.setAdapter(aa);
 
-       // aa.notifyDataSetChanged();
+       aa.notifyDataSetChanged();
 
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int
+                    position, long identity) {
+                Song song = songs.get(position);
+                Intent i = new Intent(ViewActivity.this,
+                        EditActivity.class);
+                i.putExtra("song", song);
+                startActivity(i);
+            }
+        });
+        btnView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DBHelper dbh = new DBHelper(ViewActivity.this);
+                songs = dbh.getAllSongsByStars(5);
+
+                aa.notifyDataSetChanged();
+            }
+        });
+
+    }
+protected void onResume() {
+        super.onResume();
+        DBHelper dbh = new DBHelper(ViewActivity.this);
+        songs.clear();
+        songs.addAll(dbh.getAllSongs());
+        aa.notifyDataSetChanged();
+        dbh.close();
     }
 }
